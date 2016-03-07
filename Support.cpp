20951ff868cac7d352d606327adbe87b00b2e49b
fotherja <fotherja@gmail.com>
2016-03-07 11:58:22 +0000

    Adding support files to keep Main file Cleaner
    The support files contain the pin definitions, defines and the functions:
    - Turn_Error
    - Low_Battery
    - float2s
    - Beep_Motors

---
+++ Support.cpp
@@ -0,0 +1,192 @@
+
+
+
+#include "Support.h"
+#include "Arduino.h"
+
+
+
+
+
+float Turn_Error(float Yaw_setpoint, float Yaw)
+{
+// This routine calculates the angular error to the setpoint. It returns (+ve) for clockwise or (-ve) for anticlockwise directions
+
+  float difference = Yaw_setpoint - Yaw;
+  float Error; 
+  
+  difference = abs(difference);
+
+  if(Yaw_setpoint >= 180.0 && Yaw >= 180.0)
+    {
+      if(Yaw_setpoint > Yaw) {
+        Error = Yaw_setpoint - Yaw;
+        return Error;
+      }
+      else {
+        Error = Yaw - Yaw_setpoint;
+        return -Error;
+      } 
+    }
+    
+  if(Yaw_setpoint >= 180.0 && Yaw < 180.0)
+  {
+      if(difference < 180.0) {
+        Error = Yaw_setpoint - Yaw;
+        return Error;
+      }
+      else {
+        Error = 360.0 - Yaw_setpoint + Yaw;
+        return -Error;
+      }    
+  } 
+    
+  if(Yaw_setpoint < 180.0 && Yaw < 180.0)
+  {
+      if(Yaw_setpoint > Yaw) {
+        Error = Yaw_setpoint - Yaw;
+        return Error;
+      }
+      else {
+        Error = Yaw - Yaw_setpoint;
+        return -Error; 
+      }   
+  }  
+
+  if(Yaw_setpoint < 180.0 && Yaw >= 180.0)
+  {
+      if(difference < 180.0) {
+        Error = Yaw - Yaw_setpoint;
+        return -Error;
+      }
+      else {
+        Error = 360 - Yaw + Yaw_setpoint;
+        return Error;
+      }    
+  }  
+}
+
+//--------------------------------------------------------------------------------
+int Low_Battery()
+{
+// Returns 0 if battery is sufficiently charged, 1 otherwise. Looks complicated because it also has hysterisis and fast flashes the LED to indicate the problem
+  
+  static unsigned long Time_at_LED_Change = 0;  
+  static bool Low_Battery_Flag = 0;
+  
+  float BVoltage = analogRead(Vsense) * VOLTAGE_SENSE_CONSTANT;                   //Convert to millivolts
+
+  // This section only runs the first time this routine is called. It detects how many cells the battery is made of
+  static char Number_Of_Cells = 0;
+  if(!Number_Of_Cells) {
+    if(BVoltage >= 5500)
+      Number_Of_Cells = 2;
+    else
+      Number_Of_Cells = 1;
+  }
+
+  if(Number_Of_Cells == 2)  {
+    if(BVoltage > BATTERY_THRESHOLD_LOW_2S && !Low_Battery_Flag)
+      {
+        return(0);
+      }
+    if(BVoltage > BATTERY_THRESHOLD_HGH_2S)
+      {
+        Low_Battery_Flag = 0;
+        return(0);
+      }
+  }
+  else  {
+    if(BVoltage > BATTERY_THRESHOLD_LOW_1S && !Low_Battery_Flag)
+      {
+        return(0);
+      }
+    if(BVoltage > BATTERY_THRESHOLD_HGH_1S)
+      {
+        Low_Battery_Flag = 0;
+        return(0);
+      } 
+  }   
+
+  Low_Battery_Flag = 1;  
+  DISABLE_MOTORS;                                                                 //Disable all outputs
+
+  if(millis() - Time_at_LED_Change > 100)
+  {
+    digitalWrite(LED, !digitalRead(LED));                                         // Flash LED
+    Time_at_LED_Change = millis();
+  }  
+  
+  return(1);  
+}
+
+//--------------------------------------------------------------------------------
+char * float2s(float f, unsigned int digits)
+{
+  int index = 0;
+  static char s[16];                                                              // buffer to build string representation
+ 
+  if (f < 0.0) {                                                                  // handle sign
+   s[index++] = '-';
+   f = -f;
+  } 
+  
+  if (isinf(f))  {                                                                // handle infinite values
+   strcpy(&s[index], "INF");
+   return s;
+  }
+  
+  if (isnan(f))  {                                                                // handle Not a Number
+   strcpy(&s[index], "NaN");
+   return s;
+  }
+  
+  if (digits > 6)                                                                 // max digits
+    digits = 6;
+  long multiplier = pow(10, digits);                                              // fix int => long
+  
+  int exponent = int(log10(f));
+  float g = f / pow(10, exponent);
+  if ((g < 1.0) && (g != 0.0))  {
+   g *= 10;
+   exponent--;
+  }
+  
+  long whole = long(g);                                                           // single digit
+  long part = long((g-whole)*multiplier);                                         // # digits
+  char format[16];
+  
+  sprintf(format, "%%ld.%%0%dld E%%+d", digits);
+  sprintf(&s[index], format, whole, part, exponent);
+  
+  return s;
+}
+
+//--------------------------------------------------------------------------------
+void Beep_Motors(unsigned long Frequency, unsigned long Duration)
+{  
+    ENABLE_MOTORS;
+
+    unsigned long Time_Period = 1000000 / Frequency;
+    unsigned long Start_Time = millis();
+
+    while(millis() - Start_Time < Duration)
+    {
+      OCR1A = 128 + MOTOR_BEEP_AMPLITUDE;
+      OCR1B = 128 + MOTOR_BEEP_AMPLITUDE; 
+      delayMicroseconds(Time_Period/2);   
+         
+      OCR1A = 128 - MOTOR_BEEP_AMPLITUDE;
+      OCR1B = 128 - MOTOR_BEEP_AMPLITUDE; 
+      delayMicroseconds(Time_Period/2);      
+    }
+
+    OCR1A = 128;
+    OCR1B = 128;
+    DISABLE_MOTORS;  
+}
+
+
+
+
+
---
+++ Support.h
@@ -0,0 +1,61 @@
+#ifndef _SUPPORT_H_
+#define _SUPPORT_H_
+
+
+//--- Defines: -------------------------------------------------------------------
+#define   EXPECTED_BITS               20                                          // For IR receiving. The number of bits we expect to receive
+
+#define   NO_SIGNAL_THESHOLD_PWM      10                                          // Number of erronous PPM signals received before we deduce no signal is being received
+#define   NO_SIGNAL_THESHOLD_IR       10                                          // Number of erronous PPM signals received before we deduce no signal is being received
+
+#define   VALID                       1
+#define   INVALID                     0
+
+#define   ENABLE_MOTORS               digitalWrite(MEnble, HIGH)
+#define   DISABLE_MOTORS              digitalWrite(MEnble, LOW)
+#define   LED_ON                      digitalWrite(LED, HIGH)
+#define   LED_OFF                     digitalWrite(LED, LOW)
+
+#define   MOTOR_BEEP_AMPLITUDE        100                                         // 0 - 125 full range 
+
+//--- Low_Battery() related constants:
+#define   VOLTAGE_SENSE_CONSTANT      10.08                                       // 24.71; - This is the value for the Green LED GMC that I built 1st
+#define   BATTERY_THRESHOLD_LOW_2S    6300                                        // If voltage falls below this, enter sleep
+#define   BATTERY_THRESHOLD_HGH_2S    7000                                        // If voltage rises above this, turn active again
+#define   BATTERY_THRESHOLD_LOW_1S    3150                                        // If voltage falls below this, enter sleep
+#define   BATTERY_THRESHOLD_HGH_1S    3500                                        // If voltage rises above this, turn active again
+
+
+//--- Pin definitions: -----------------------------------------------------------
+#define   ESC_PWM_Pin       3                                                     // Output PWM, used if IR detected and hence FwdBckIn not being used etc
+
+#define   FwdBckIn          3                                                     // This is INT1
+#define   LfRghtIn          1                                                     // This is PCINT17 (calls PCINT2_vect). Although this is also the Tx pin
+
+#define   MEnble            8                                                     // Enable Motors
+#define   MotorL            9                                                     // Left Motors
+#define   MotorR            10                                                    // Right Motors
+
+#define   IR_Pos            13                                                    // Convienient use of pins to supply power to IR reciever.
+#define   IR_GND            12
+#define   IR_In             11                                                    // PB3 which is PCINT3
+
+#define   LED               0                                                     // LED Pin. Although this is also the Rx pin
+#define   Vsense            A2                                                    // Voltage sensing (150ohm to 1K divider)
+
+
+//--- Routines -------------------------------------------------------------------
+float Turn_Error(float, float);                                                   // From the Yaw & Yaw_setpoint, this routine returns how many degrees we are from where we want to be. 
+int Low_Battery(void);                                                            // Returns 0 if battery is sufficiently charged, 1 otherwise. Includes hysterisis & LED flashing for low battery (Non-blocking)
+char * float2s(float f, unsigned int digits);
+void Beep_Motors(unsigned long Frequency, unsigned long Duration);                // Uses the motors as speakers to produce noise
+
+
+
+#endif 
+
+
+
+
+
+
